b5f226d7c8c243510cbb5cfd2ee3d7163f94f56e
killiancolla <killiancolla@hotmail.fr>
2024-06-04 22:46:13 +0200

    correction deploy

--- src/app/dc/page.tsx
+++ src/app/dc/page.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 
 import Cookies from "js-cookie";
 import { useRouter } from 'next/navigation';
 import { useEffect } from "react";
 
-export default function handleLogout() {
-
+const HandleLogout = () => {
     const router = useRouter();
 
     useEffect(() => {
         Cookies.remove('token');
         router.push('/login');
-    }, [])
+    }, [router]);
+
+    return null;
 };
+
+export default HandleLogout;
